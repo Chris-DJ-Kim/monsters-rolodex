195bb210aad4cf1bdf842d3b67d68106ad8884f9
Chris Kim <chrisdjkim@gmail.com>
2020-06-03 23:55:12 -0400

    Updates

---
+++ precache-manifest.a4f88a78b1377210b9cdc76d4ee8b67c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51c49c1c235d0051ed59032ef6975d1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac802e553cee609537e4",
+    "url": "/monsters-rolodex/static/css/main.51a5facb.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac802e553cee609537e4",
+    "url": "/monsters-rolodex/static/js/main.738e858e.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
