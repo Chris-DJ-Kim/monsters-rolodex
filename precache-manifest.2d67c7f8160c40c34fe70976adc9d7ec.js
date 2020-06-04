64bbccc9075e775441d8ea0dd7e2c613eb541f5a
Chris Kim <chrisdjkim@gmail.com>
2020-06-03 23:18:22 -0400

    Updates

---
+++ precache-manifest.2d67c7f8160c40c34fe70976adc9d7ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0695aecf1e65ce397b2401936b6103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05b6933770c041b1a8fe",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3103ab1e3a9e55a70d11",
+    "url": "/static/js/2.6c832960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c832960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b6933770c041b1a8fe",
+    "url": "/static/js/main.734267b9.chunk.js"
+  },
+  {
+    "revision": "321be40e4d0b3008f64a",
+    "url": "/static/js/runtime-main.c34bb27e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
